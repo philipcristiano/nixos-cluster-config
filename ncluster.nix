07dc317789f29a0989c9ad85151ebd3af7cf31d5
Philip Cristiano <git@philipcristiano.com>
2022-03-08 19:48:28 -0500

    Create consul cluster
    Should document generating and setting certs

--- ncluster.nix
+++ ncluster.nix
@@ -3,7 +3,24 @@
 { environment.systemPackages = [ pkgs.consul
                                  pkgs.nomad
                                  pkgs.vault];
-  # services.consul.enable = true;
+  services.consul.enable = true;
+  services.consul.extraConfig = {
+        server = true;
+        bootstrap_expect = 3;
+        ca_file = "/etc/consul.d/consul-agent-ca.pem";
+        cert_file = "/etc/consul.d/dc1-server-consul.pem";
+        key_file = "/etc/consul.d/dc1-server-consul-key.pem";
+        auto_encrypt = {
+            allow_tls = true;
+        };
+        retry_join     = [ "192.168.102.100" "192.168.102.101" "192.168.102.102" ];
+        acl  = {
+            enabled = false;
+            default_policy = "allow";
+            enable_token_persistence = true;
+        };
+  };
+
   # systemd.services.consul.serviceConfig.Type = "notify";
 
   services.nomad = {
@@ -23,8 +40,10 @@
         };
     };
   };
+  services.consul.interface.bind = "enp2s0";
   services.nomad.enable = true;
+  networking.firewall.allowedTCPPorts = [ 8300 8301 8500 8600 ];
   networking.firewall.allowedTCPPortRanges = [
     { from = 4646; to = 4648; }
-];
+  ];
 }
