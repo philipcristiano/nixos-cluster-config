50691a16052dae6faef5c7f6c279bfed2fe03904
Philip Cristiano <git@philipcristiano.com>
2022-09-05 18:51:07 -0400

    firewall: Allow LoRaWAN

--- ncluster.nix
+++ ncluster.nix
@@ -122,6 +122,7 @@ in
   services.consul.interface.bind = "enp2s0";
   services.nomad.enable = true;
 
+  networking.firewall.allowedUDPPorts = [ 1680 1700 ];
   networking.firewall.allowedTCPPorts = [ 80 443 1883 8300 8301 8500 8600 ];
   networking.firewall.allowedTCPPortRanges = [
     { from = 4646; to = 4648; }
