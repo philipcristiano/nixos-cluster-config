ea844d1fff7c23eaf532f197c31cfa4be5158ef2
Philip Cristiano <git@philipcristiano.com>
2023-07-25 19:04:17 -0400

    ytdl-sub: fix tag format

--- services/ytdl-sub/deploy.sh
+++ services/ytdl-sub/deploy.sh
@@ -6,7 +6,7 @@ vault policy write service-ytdl-sub policy.vault
 # nomad volume create ytdl-sub.volume
 
 ## ubuntu-latest as of 2023-07-18
-export NOMAD_VAR_image_id="ghcr.io/jmbannon/ytdl-sha256:169a20b0f61ff27270c620af47b9818a16590910b01d905ee396f13b8ef4faa7"
+export NOMAD_VAR_image_id="ghcr.io/jmbannon/ytdl-sub@sha256:169a20b0f61ff27270c620af47b9818a16590910b01d905ee396f13b8ef4faa7"
 
 nomad run ytdl-sub.nomad
 nomad run ytdl-sub-once.nomad
