4444d5a60e16696e59b1a5297c0d6b7d273b5faa
Philip Cristiano <git@philipcristiano.com>
2023-08-30 10:08:25 -0400

    baserow: Deploy backup

--- services/baserow/deploy.sh
+++ services/baserow/deploy.sh
@@ -3,6 +3,7 @@ set -ex
 vault policy write service-baserow policy.vault
 
 #nomad volume create baserow-postgres.volume
+nomad run baserow-postgres-backup.nomad
 nomad run baserow-postgres.nomad
 nomad run baserow-redis.nomad
 nomad run baserow.nomad
