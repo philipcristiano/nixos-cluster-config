57d8a0038f8a92c701dd5ebedbd5b9bc92e64293
Philip Cristiano <git@philipcristiano.com>
2023-09-28 19:26:25 -0400

    ytdl-sub:ubuntu-2023.09.28

--- services/ytdl-sub/deploy.sh
+++ services/ytdl-sub/deploy.sh
@@ -5,7 +5,7 @@ vault policy write service-ytdl-sub policy.vault
 # nomad volume create yt_tvshows.volume
 # nomad volume create ytdl-sub.volume
 
-export NOMAD_VAR_image_id="ghcr.io/jmbannon/ytdl-sub@ubuntu-2023.09.22.post5"
+export NOMAD_VAR_image_id="ghcr.io/jmbannon/ytdl-sub:ubuntu-2023.09.28"
 
 nomad run ytdl-sub.nomad
 nomad run ytdl-sub-once.nomad
