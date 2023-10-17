795184b5f40600271ef0cd12a9af987284e740d6
Philip Cristiano <git@philipcristiano.com>
2023-10-17 11:01:59 -0400

    ytd-dl: Move var to file instead of shell

--- services/ytdl-sub/deploy.sh
+++ services/ytdl-sub/deploy.sh
@@ -5,7 +5,5 @@ vault policy write service-ytdl-sub policy.vault
 # nomad volume create yt_tvshows.volume
 # nomad volume create ytdl-sub.volume
 
-export NOMAD_VAR_image_id="jmbannon/ytdl-sub:ubuntu-2023.10.03"
-
-nomad run -var-file=../../nomad_job.vars ytdl-sub.nomad
-nomad run -var-file=../../nomad_job.vars ytdl-sub-once.nomad
+nomad run -var-file=../../nomad_job.vars -var-file=./service.vars ytdl-sub.nomad
+nomad run -var-file=../../nomad_job.vars -var-file=./service.vars ytdl-sub-once.nomad
