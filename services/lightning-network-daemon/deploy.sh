61d08c65a9d8af6b25e7d61def55ba9250ac44f1
Philip Cristiano <git@philipcristiano.com>
2023-10-18 14:04:17 -0400

    lightning: Add lndmon
    Bump versions
    Split out config templates into separate files

--- services/lightning-network-daemon/deploy.sh
+++ services/lightning-network-daemon/deploy.sh
@@ -3,4 +3,4 @@ set -ex
 vault policy write service-lightning-network-daemon policy.vault
 vault policy write service-lightning-terminal policy.vault
 # nomad volume create lightning-network-daemon.volume
-nomad run lightning-network-daemon.nomad
+nomad run -var-file=../../nomad_job.vars lightning-network-daemon.nomad
