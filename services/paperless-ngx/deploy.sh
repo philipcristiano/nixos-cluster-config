85682ee712cd37259d77b257ca67bd807adcbcb8
Philip Cristiano <git@philipcristiano.com>
2024-01-30 10:26:30 -0500

    paperless-ngx:2.4.3
    Also trigger dependency deploys

--- services/paperless-ngx/deploy.sh
+++ services/paperless-ngx/deploy.sh
@@ -2,5 +2,13 @@ set -ex
 
 vault policy write service-paperless-ngx policy.vault
 
+pushd ../neon-compute
+bash deploy.sh paperless-ngx
+popd
+
+pushd ../redis
+bash deploy.sh paperless-ngx
+popd
+
 # nomad volume create paperless-ngx.volume
 nomad run -var-file=../../nomad_job.vars paperless-ngx.nomad
