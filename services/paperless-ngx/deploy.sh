868f5239abaf0a11119b30d4804979235dbdbd42
Philip Cristiano <git@philipcristiano.com>
2024-02-16 12:39:18 -0500

    paperless-ngx: Deploy from Dockerfile / fetch image automagically

--- services/paperless-ngx/deploy.sh
+++ services/paperless-ngx/deploy.sh
@@ -2,6 +2,9 @@ set -ex
 
 vault policy write service-paperless-ngx policy.vault
 
+SERVICE_ID=paperless-ngx
+IMAGE_ID=$(awk '/FROM/ {print $2}' Dockerfile)
+
 pushd ../neon-compute
 bash deploy.sh paperless-ngx
 popd
@@ -10,5 +13,6 @@ pushd ../redis
 bash deploy.sh paperless-ngx
 popd
 
+nomad job dispatch -meta image="${IMAGE_ID}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
 # nomad volume create paperless-ngx.volume
-nomad run -var-file=../../nomad_job.vars paperless-ngx.nomad
+nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" paperless-ngx.nomad
--- services/paperless-ngx/paperless-ngx.nomad
+++ services/paperless-ngx/paperless-ngx.nomad
@@ -12,7 +12,6 @@ variable "domain" {
 variable "image_id" {
   type        = string
   description = "The docker image used for task."
-  default     = "paperlessngx/paperless-ngx:2.5.2"
 }
 
 job "paperless-ngx" {
