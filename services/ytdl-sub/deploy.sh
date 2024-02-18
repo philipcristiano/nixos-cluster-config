1d15a139077a10311861597a82cc987ec454b0f6
Philip Cristiano <git@philipcristiano.com>
2024-02-18 17:44:07 -0500

    ytdl-sub: Update deploy

--- services/ytdl-sub/deploy.sh
+++ services/ytdl-sub/deploy.sh
@@ -2,8 +2,15 @@ set -ex
 
 vault policy write service-ytdl-sub policy.vault
 
+SERVICE_ID=ytdl-sub
+IMAGE_ID=$(awk '/FROM ./ {sub(/.[^\/]*\//, "", $2 ); print $2}' Dockerfile)
+SOURCE_REGISTRY="$(awk '/FROM ./ {sub(/\/.*/, "", $2 ); print $2}' Dockerfile)/"
+
+
 # nomad volume create yt_tvshows.volume
 # nomad volume create ytdl-sub.volume
+#
+nomad job dispatch -meta image="${IMAGE_ID}" -meta source_registry="${SOURCE_REGISTRY}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
 
-nomad run -var-file=../../nomad_job.vars -var-file=./service.vars ytdl-sub.nomad
-nomad run -var-file=../../nomad_job.vars -var-file=./service.vars ytdl-sub-once.nomad
+nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" ytdl-sub.nomad
+nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" ytdl-sub-once.nomad
