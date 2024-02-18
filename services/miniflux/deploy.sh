f11347deb451905188e0722ef9dec8452a74dddf
Philip Cristiano <git@philipcristiano.com>
2024-02-18 18:08:23 -0500

    miniflux: Update deploy

--- services/miniflux/deploy.sh
+++ services/miniflux/deploy.sh
@@ -2,4 +2,15 @@ set -ex
 
 vault policy write service-miniflux policy.vault
 
-nomad run -var-file=../../nomad_job.vars -var-file=./service.vars miniflux.nomad
+
+SERVICE_ID=miniflux
+IMAGE_ID=$(awk '/FROM ./ {sub(/.[^\/]*\//, "", $2 ); print $2}' Dockerfile)
+SOURCE_REGISTRY="$(awk '/FROM ./ {sub(/\/.*/, "", $2 ); print $2}' Dockerfile)/"
+
+pushd ../neon-compute
+bash deploy.sh miniflux
+popd
+
+nomad job dispatch -meta image="${IMAGE_ID}" -meta source_registry="${SOURCE_REGISTRY}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
+
+nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}"  miniflux.nomad
--- services/miniflux/miniflux.nomad
+++ services/miniflux/miniflux.nomad
@@ -12,7 +12,6 @@ variable "domain" {
 variable "image_id" {
   type        = string
   description = "The docker image used for task."
-  default     = "miniflux/miniflux:2.0.51"
 }
 
 variable "count" {
