4364e79f1adba2e25f862de16f3a26b57f6a981a
Philip Cristiano <git@philipcristiano.com>
2024-04-18 13:07:13 -0400

    baserow: Include dockerfile/redis

--- services/baserow/deploy.sh
+++ services/baserow/deploy.sh
@@ -12,5 +12,9 @@ pushd ../postgres
 bash deploy.sh baserow
 popd
 
+pushd ../redis
+bash deploy.sh baserow
+popd
+
 nomad job dispatch -meta image="${IMAGE_ID}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
 nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" "${SERVICE_ID}.nomad"
