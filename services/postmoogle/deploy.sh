76587e8cfe70116c20f8198ee64deac138a31318
Philip Cristiano <git@philipcristiano.com>
2024-05-02 10:19:56 -0400

    postmoogle: Use gitlab repo

--- services/postmoogle/deploy.sh
+++ services/postmoogle/deploy.sh
@@ -1,9 +1,11 @@
 set -ex
 
 SERVICE_ID=postmoogle
-IMAGE_ID=$(awk '/FROM/ {print $2}' Dockerfile)
+IMAGE_ID=$(awk '/FROM ./ {sub(/.[^\/]*\//, "", $2 ); print $2}' Dockerfile)
+SOURCE_REGISTRY="$(awk '/FROM ./ {sub(/\/.*/, "", $2 ); print $2}' Dockerfile)/"
 
 vault policy write "service-${SERVICE_ID}" policy.vault
 
-nomad job dispatch -meta image="${IMAGE_ID}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
+nomad job dispatch -meta image="${IMAGE_ID}" -meta source_registry="${SOURCE_REGISTRY}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
+
 nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" "${SERVICE_ID}.nomad"
