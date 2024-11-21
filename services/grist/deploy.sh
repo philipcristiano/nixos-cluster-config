e5853ee18f9e219b5ba26199074bb7f6e71b98ca
Philip Cristiano <git@philipcristiano.com>
2024-11-21 15:12:40 -0500

    service: Add grist

---
+++ services/grist/deploy.sh
@@ -0,0 +1,13 @@
+set -ex
+
+SERVICE_ID=grist
+IMAGE_ID=$(awk '/FROM/ {print $2}' Dockerfile)
+
+vault policy write "service-${SERVICE_ID}" policy.vault
+
+pushd ../postgres
+bash deploy.sh "${SERVICE_ID}"
+popd
+
+nomad job dispatch -meta image="${IMAGE_ID}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
+nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" "${SERVICE_ID}.nomad"
