677cbf1440821663b3275dd48a07e362daa232b0
Philip Cristiano <git@philipcristiano.com>
2025-05-22 10:42:39 -0400

    Remove nostr services
    I no longer use them

--- services/nostress/deploy.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-
-vault policy write service-nostress policy.vault
-
-SERVICE_ID=nostress
-IMAGE_ID=$(awk '/FROM/ {print $2}' Dockerfile)
-
-nomad job dispatch -meta image="${IMAGE_ID}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
-nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" nostress.nomad
