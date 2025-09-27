9722e2f318ac359c8fd43e88d3e4dc23af2197db
Philip Cristiano <git@philipcristiano.com>
2025-09-27 08:20:36 -0400

    chore: Cleanup unused services

--- services/electrs/deploy.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-
-SERVICE_ID=electrs
-IMAGE_ID=$(awk '/FROM/ {print $2}' Dockerfile)
-
-vault policy write "service-${SERVICE_ID}" policy.vault
-
-nomad job dispatch -meta image="${IMAGE_ID}" -id-prefix-template="${SERVICE_ID}" regctl-img-copy
-nomad run -var-file=../../nomad_job.vars -var "image_id=${IMAGE_ID}" "${SERVICE_ID}.nomad"
--- services/freshrss/README.md
+++
@@ -1,8 +0,0 @@
-
-
-# OIDC
-
-kanidm system oauth2 create freshrss "FreshRSS" <origin>
-kanidm system oauth2 update-scope-map freshrss <group> openid
-kanidm system oauth2 prefer-short-username freshrss
-kanidm system oauth2 warning-insecure-client-disable-pkce freshrss
